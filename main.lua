fb62e748d2b2f0a2f60a0d2bae1b8b2558311b25
Amalia Hawkins <amalia.hawkins@10gen.com>
2015-01-24 09:43:07 -0500

    Starting work on spaceship logic for disasteroids mini game.

--- main.lua
+++ main.lua
@@ -1,7 +1,17 @@
 function love.load()
     -- love calls this once, on game startup.
     -- this is the place to load resources, initialize variables, etc.
+
+    -- 1. load required files.
+    require 'src.disasteroids'
+
+    -- 2. initialize global variables.
     gameIsPaused = false
+    midpointX = love.graphics.getWidth()/2
+    midpointY = love.graphics.getHeight()/2
+    disasteroidsActive = true -- TODO switch to false
+    diasteroidsInit(midpointX, midpointY) -- TODO move to minigame startup
+
     -- TODO
 end
 
@@ -20,8 +30,8 @@ function love.draw()
     -- this is where all the drawing happens.
     -- all love.graphics functions need to be called from within this function.
     if gameIsPaused then
-        love.graphics.print("Game Paused", 400, 300)
-        return
+        love.graphics.print("Game Paused", midpointX - 45, midpointY - 50)
+        return -- TODO maybe don't return? instead, print this last?
     end
     -- TODO
 end
@@ -38,8 +48,15 @@ function love.keypressed(key)
     -- this is called whenever a key is pressed.
     if key == 'p' then
         gameIsPaused = not gameIsPaused
+    elseif key == 'a' and disasteroidsActive then
+        spaceshipTurn('left')
+    elseif key == 'd' and disasteroidsActive then
+        spaceshipTurn('right')
+    elseif key == 'w' and disasteroidsActive then
+        -- TODO
+    elseif key == 's' and diasteroidsActive then
+        -- TODO
     end
-    -- TODO
 end
 
 function love.keyreleased(key)
